ae1766721e9755a4b58c0411826edd949c480a53
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2023-03-23 21:34:55 +0000

    ES6_basic

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+
+rt default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
