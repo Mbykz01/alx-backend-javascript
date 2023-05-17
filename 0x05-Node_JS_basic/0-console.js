d68c34d03fbdb220826cabfdf5f8ee008fab4168
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2023-05-17 04:57:29 +0000

    0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,10 @@
 /**
- *  * Displays a message on the STDOUT.
- *   * @param {String} msg The message to display.
- *    * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- *     */
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 const displayMessage = (msg) => {
-	  console.log(msg);
+  console.log(msg);
 };
 
 module.exports = displayMessage;
