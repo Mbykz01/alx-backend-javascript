8dd3d4df4b2fd58a69f0fff5cee373460550a4c8
Mbykz01 <aminuabdulkarimibrahimu@gmail.com>
2023-06-23 14:55:42 +0000

    createIterator

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,8 @@
 export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
-    }
-  }());
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
+
+  return myarray;
 }
